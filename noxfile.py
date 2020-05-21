66bee11d748b4c74f6c539edf112986a094e6426
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2020-05-21 05:46:35 +0200

    Use this project in its own noxfile.py

--- noxfile.py
+++ noxfile.py
@@ -1,114 +1,22 @@
 """Nox sessions."""
-import contextlib
 import shutil
 import sys
-import tempfile
 from pathlib import Path
 from textwrap import dedent
-from typing import cast
-from typing import Iterator
 
 import nox
 from nox.sessions import Session
+
+sys.path.insert(0, "src")
+
+from nox_poetry import _Poetry
+from nox_poetry import install
+from nox_poetry import install_package
 
 
 package = "nox_poetry"
 python_versions = ["3.8", "3.7", "3.6"]
 nox.options.sessions = "pre-commit", "safety", "mypy", "tests", "typeguard"
-
-
-class Poetry:
-    """Helper class for invoking Poetry inside a Nox session.
-
-    Attributes:
-        session: The Session object.
-    """
-
-    def __init__(self, session: Session) -> None:
-        """Constructor."""
-        self.session = session
-
-    @contextlib.contextmanager
-    def export(self, *args: str) -> Iterator[Path]:
-        """Export the lock file to requirements format.
-
-        Args:
-            args: Command-line arguments for ``poetry export``.
-
-        Yields:
-            The path to the requirements file.
-        """
-        with tempfile.TemporaryDirectory() as directory:
-            requirements = Path(directory) / "requirements.txt"
-            self.session.run(
-                "poetry",
-                "export",
-                *args,
-                "--format=requirements.txt",
-                f"--output={requirements}",
-                external=True,
-            )
-            yield requirements
-
-    def version(self) -> str:
-        """Retrieve the package version.
-
-        Returns:
-            The package version.
-        """
-        output = self.session.run(
-            "poetry", "version", external=True, silent=True, stderr=None
-        )
-        return cast(str, output).split()[1]
-
-    def build(self, *args: str) -> None:
-        """Build the package.
-
-        Args:
-            args: Command-line arguments for ``poetry build``.
-        """
-        self.session.run("poetry", "build", *args, external=True)
-
-
-def install_package(session: Session) -> None:
-    """Build and install the package.
-
-    Build a wheel from the package, and install it into the virtual environment
-    of the specified Nox session.
-
-    The package requirements are installed using the versions specified in
-    Poetry's lock file.
-
-    Args:
-        session: The Session object.
-    """
-    poetry = Poetry(session)
-
-    with poetry.export() as requirements:
-        session.install(f"--requirement={requirements}")
-
-    poetry.build("--format=wheel")
-
-    version = poetry.version()
-    session.install(
-        "--no-deps", "--force-reinstall", f"dist/{package}-{version}-py3-none-any.whl"
-    )
-
-
-def install(session: Session, *args: str) -> None:
-    """Install development dependencies into the session's virtual environment.
-
-    This function is a wrapper for nox.sessions.Session.install.
-
-    The packages must be managed as development dependencies in Poetry.
-
-    Args:
-        session: The Session object.
-        args: Command-line arguments for ``pip install``.
-    """
-    poetry = Poetry(session)
-    with poetry.export("--dev") as requirements:
-        session.install(f"--constraint={requirements}", *args)
 
 
 def activate_virtualenv_in_precommit_hooks(session: Session) -> None:
@@ -188,7 +96,7 @@ def precommit(session: Session) -> None:
 @nox.session(python="3.8")
 def safety(session: Session) -> None:
     """Scan dependencies for insecure packages."""
-    poetry = Poetry(session)
+    poetry = _Poetry(session)
     with poetry.export("--dev", "--without-hashes") as requirements:
         install(session, "safety")
         session.run("safety", "check", f"--file={requirements}", "--bare")
