7724c6d9fcf986f5571e2d3c7a99d1a723439800
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2020-09-26 22:14:05 +0200

    Export WHEEL and SDIST unqualified

--- src/nox_poetry/__init__.py
+++ src/nox_poetry/__init__.py
@@ -1,10 +1,15 @@
 """Using Poetry in Nox sessions."""
 from nox_poetry.core import export_requirements  # noqa: F401
 from nox_poetry.core import install  # noqa: F401
-from nox_poetry.core import PackageType  # noqa: F401
+from nox_poetry.core import PackageType
+
+
+WHEEL = PackageType.WHEEL
+SDIST = PackageType.SDIST
 
 __all__ = [
     "export_requirements",
     "install",
-    "PackageType",
+    "WHEEL",
+    "SDIST",
 ]
