f7c3b080d725f98341ecaccbbcaac3c925e5bbe2
Claudio Jolowicz <claudio.jolowicz@cyren.com>
2020-09-27 09:35:07 +0200

    Remove redundant noqa: F401

--- src/nox_poetry/__init__.py
+++ src/nox_poetry/__init__.py
@@ -1,6 +1,6 @@
 """Using Poetry in Nox sessions."""
-from nox_poetry.core import export_requirements  # noqa: F401
-from nox_poetry.core import install  # noqa: F401
+from nox_poetry.core import export_requirements
+from nox_poetry.core import install
 from nox_poetry.poetry import DistributionFormat
 
 
