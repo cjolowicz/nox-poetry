144a44b8b9d6ba0365c4d03107c5ad6179bf3b51
Claudio Jolowicz <mail@claudiojolowicz.com>
2020-11-14 13:24:27 +0100

    Update cookiecutter-hypermodern-python to e7f0a16 (#162)

--- docs/conf.py
+++ docs/conf.py
@@ -5,7 +5,10 @@ from datetime import datetime
 project = "nox-poetry"
 author = "Claudio Jolowicz"
 copyright = f"{datetime.now().year}, {author}"
-extensions = ["sphinx.ext.autodoc", "sphinx.ext.napoleon"]
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+]
 autodoc_typehints = "description"
 html_theme = "furo"
 html_favicon = "images/favicon.png"
