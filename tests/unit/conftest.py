a0ace67390f0d731de1f73f835ccd86905e185fd
Claudio Jolowicz <mail@claudiojolowicz.com>
2020-12-01 23:33:03 +0100

    Move fixtures to conftest.py

---
+++ tests/unit/conftest.py
@@ -0,0 +1,38 @@
+"""Fixtures."""
+from pathlib import Path
+from typing import Any
+from typing import cast
+
+import pytest
+from _pytest.monkeypatch import MonkeyPatch
+from nox.sessions import Session
+
+
+class FakeSession:
+    """Fake session."""
+
+    def __init__(self, path: Path) -> None:
+        """Initialize."""
+        self.path = path
+
+    def run(self, *args: str, **kargs: Any) -> str:
+        """Run."""
+        path = Path("dist") / "example.whl"
+        path.touch()
+        return path.name
+
+    def install(self, *args: str, **kargs: Any) -> None:
+        """Install."""
+        pass
+
+    def create_tmp(self, *args: str, **kargs: Any) -> str:
+        """Create temporary directory."""
+        return str(self.path)
+
+
+@pytest.fixture
+def session(tmp_path: Path, monkeypatch: MonkeyPatch) -> Session:
+    """Return a fake Nox session."""
+    monkeypatch.setattr("nox_poetry.core.Session_install", FakeSession.install)
+    session = FakeSession(tmp_path)
+    return cast(Session, session)
--- tests/unit/test_nox_poetry.py
+++ tests/unit/test_nox_poetry.py
@@ -1,43 +1,7 @@
 """Unit tests."""
-from pathlib import Path
-from typing import Any
-from typing import cast
-
-import pytest
-from _pytest.monkeypatch import MonkeyPatch
 from nox.sessions import Session
 
 import nox_poetry
-
-
-class FakeSession:
-    """Fake session."""
-
-    def __init__(self, path: Path) -> None:
-        """Initialize."""
-        self.path = path
-
-    def run(self, *args: str, **kargs: Any) -> str:
-        """Run."""
-        path = Path("dist") / "example.whl"
-        path.touch()
-        return path.name
-
-    def install(self, *args: str, **kargs: Any) -> None:
-        """Install."""
-        pass
-
-    def create_tmp(self, *args: str, **kargs: Any) -> str:
-        """Create temporary directory."""
-        return str(self.path)
-
-
-@pytest.fixture
-def session(tmp_path: Path, monkeypatch: MonkeyPatch) -> Session:
-    """Return a fake Nox session."""
-    monkeypatch.setattr("nox_poetry.core.Session_install", FakeSession.install)
-    session = FakeSession(tmp_path)
-    return cast(Session, session)
 
 
 def test_install(session: Session) -> None:
