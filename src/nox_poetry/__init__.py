84c4195d030d524bc552579a8096d1e490afffab
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-02-01 23:43:58 +0100

    feat: Export @session and Session from package

--- src/nox_poetry/__init__.py
+++ src/nox_poetry/__init__.py
@@ -1,15 +1,25 @@
 """Using Poetry in Nox sessions.
 
-This package provides a facility to monkey-patch Nox so ``session.install``
-installs packages at the versions specified in the Poetry lock file, and
-``"."`` is replaced by a wheel built from the local package.
-See :mod:`nox_poetry.patch`.
+This package provides a drop-in replacement for the :func:`session` decorator,
+and for the :class:`Session` object passed to user-defined session functions.
+This enables :meth:`session.install
+<nox_poetry.sessions._PoetrySession.install>` to install packages at the
+versions specified in the Poetry lock file.
+
+Example:
+    >>> @session(python=["3.8", "3.9"])
+    ... def tests(session: Session) -> None:
+    ...     session.install("pytest", ".")
+    ...     session.run("pytest")
 
 It also provides helper functions that allow more fine-grained control:
 
-- :func:`install`
-- :func:`build_package`
-- :func:`export_requirements`
+- :meth:`session.poetry.installroot
+  <nox_poetry.sessions._PoetrySession.installroot>`
+- :meth:`session.poetry.build_package
+  <nox_poetry.sessions._PoetrySession.build_package>`
+- :meth:`session.poetry.export_requirements
+  <nox_poetry.sessions._PoetrySession.export_requirements>`
 
 Two constants are defined to specify the format for distribution archives:
 
@@ -21,6 +31,8 @@ from nox_poetry.core import export_requirements
 from nox_poetry.core import install
 from nox_poetry.core import installroot
 from nox_poetry.poetry import DistributionFormat
+from nox_poetry.sessions import Session
+from nox_poetry.sessions import session
 
 
 #: A wheel archive.
@@ -34,6 +46,8 @@ __all__ = [
     "export_requirements",
     "install",
     "installroot",
+    "Session",
+    "session",
     "SDIST",
     "WHEEL",
 ]
