8922259ae264e02d64cd8c6f0224b5f386aab93b
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-02-04 13:12:37 +0100

    docs: Deprecate patch module

--- src/nox_poetry/patch.py
+++ src/nox_poetry/patch.py
@@ -1,5 +1,8 @@
 """Monkey-patch Nox to install packages using nox-poetry.
 
+.. deprecated:: 0.8
+   Use :func:`session` instead.
+
 Import this module to monkey-patch Nox.
 See :func:`nox_poetry.core.patch` for details.
 """
