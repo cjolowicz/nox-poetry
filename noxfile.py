55ba15a3ab45d24cbbaa35d824cbf3a794becf6f
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-06-11 13:14:06 +0200

    ci: Require nox >= 2021.6.6 in Noxfile

--- noxfile.py
+++ noxfile.py
@@ -13,6 +13,7 @@ from nox_poetry import session
 
 package = "nox_poetry"
 python_versions = ["3.9", "3.8", "3.7", "3.6"]
+nox.needs_version = ">= 2021.6.6"
 nox.options.sessions = (
     "pre-commit",
     "safety",
