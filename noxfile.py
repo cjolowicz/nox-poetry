f54d16ffb24ca76bbf4a858276d65a23f3328744
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-11-12 12:32:59 +0100

    👷 [pre-commit] Add pyupgrade hook (#514)
    * ⬆ [poetry] Add pyupgrade ^2.29.0 to development dependencies
    
    * 🔧 [nox] Install pyupgrade in pre-commit session
    
    * 🔧 [pre-commit] Add pyupgrade hook
    
    * 🔨 [functional] Modernize Python 3.6-compatible `subprocess.run` calls

--- noxfile.py
+++ noxfile.py
@@ -90,6 +90,7 @@ def precommit(session: Session) -> None:
         "pre-commit",
         "pre-commit-hooks",
         "reorder-python-imports",
+        "pyupgrade",
     )
     session.run("pre-commit", *args)
     if args and args[0] == "install":
