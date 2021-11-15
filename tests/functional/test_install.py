1410253b8e009251f7a691b1f727ad5070809b86
Claudio Jolowicz <mail@claudiojolowicz.com>
2021-11-15 11:38:56 +0100

    🔨 [functional] Split functional tests into multiple modules (#522)
    * 🔨 [functional] Share test data between `tests.functional` modules
    
    * 🔨 [functional] Move `patch` tests to separate module
    
    * 🔨 [functional] Rename tests removing `without_patch` suffix
    
    * 🔨 [functional] Move `installroot` tests to separate module
    
    * 🔨 [functional] Move `install` tests to separate module
    
    * 🔨 [functional] Rename module with `@session` tests
    
    * 🔨 [functional] Rename `installroot` tests
    
    * 🔨 [functional] Rename `install` tests
    
    * 🔨 [functional] Rename `patch` tests
    
    * 🔨 [functional] Rename `@session` tests
    
    * 🔨 [functional] Move test from `test_installroot` to `test_sessions`
    
    * 🔨 [functional] Rename `@session` tests
    
    * 🔨 [functional] Move `session.poetry` tests to separate module

---
+++ tests/functional/test_install.py
@@ -0,0 +1,79 @@
+"""Functional tests for ``install``."""
+import nox.sessions
+from tests.functional.conftest import list_packages
+from tests.functional.conftest import Project
+from tests.functional.conftest import run_nox_with_noxfile
+
+import nox_poetry
+
+
+def test_local_wheel(project: Project) -> None:
+    """It builds and installs a wheel from the local package."""
+
+    @nox.session
+    def test(session: nox.sessions.Session) -> None:
+        """Install the local package."""
+        nox_poetry.install(session, ".")
+
+    run_nox_with_noxfile(project, [test], [nox, nox.sessions, nox_poetry])
+
+    expected = [project.package, *project.dependencies]
+    packages = list_packages(project, test)
+
+    assert set(expected) == set(packages)
+
+
+def test_local_wheel_with_extras(project: Project) -> None:
+    """It installs the extra."""
+
+    @nox.session
+    def test(session: nox.sessions.Session) -> None:
+        """Install the local package."""
+        nox_poetry.install(session, ".[pygments]")
+
+    run_nox_with_noxfile(project, [test], [nox, nox.sessions, nox_poetry])
+
+    expected = [
+        project.package,
+        *project.dependencies,
+        project.get_dependency("pygments"),
+    ]
+    packages = list_packages(project, test)
+
+    assert set(expected) == set(packages)
+
+
+def test_dependency(project: Project) -> None:
+    """It installs the pinned dependency."""
+
+    @nox.session
+    def test(session: nox.sessions.Session) -> None:
+        """Install the dependency."""
+        nox_poetry.install(session, "pycodestyle")
+
+    run_nox_with_noxfile(project, [test], [nox, nox.sessions, nox_poetry])
+
+    expected = [project.get_dependency("pycodestyle")]
+    packages = list_packages(project, test)
+
+    assert set(expected) == set(packages)
+
+
+def test_local_wheel_and_dependency(project: Project) -> None:
+    """It installs the wheel with pinned dependencies."""
+
+    @nox.session
+    def test(session: nox.sessions.Session) -> None:
+        """Install the dependency."""
+        nox_poetry.install(session, ".", "pycodestyle")
+
+    run_nox_with_noxfile(project, [test], [nox, nox.sessions, nox_poetry])
+
+    expected = [
+        project.package,
+        *project.dependencies,
+        project.get_dependency("pycodestyle"),
+    ]
+    packages = list_packages(project, test)
+
+    assert set(expected) == set(packages)
---
+++ tests/functional/test_session.py
@@ -0,0 +1,160 @@
+"""Functional tests for the `@session` decorator."""
+from pathlib import Path
+
+import nox.sessions
+import pytest
+from tests.functional.conftest import list_packages
+from tests.functional.conftest import Project
+from tests.functional.conftest import run_nox_with_noxfile
+
+import nox_poetry.patch
+
+
+def test_local(project: Project) -> None:
+    """It installs the local package."""
+
+    @nox_poetry.session
+    def test(session: nox_poetry.Session) -> None:
+        """Install the local package."""
+        session.install(".")
+
+    run_nox_with_noxfile(project, [test], [nox_poetry])
+
+    expected = [project.package, *project.dependencies]
+    packages = list_packages(project, test)
+
+    assert set(expected) == set(packages)
+
+
+def test_local_with_extras(project: Project) -> None:
+    """It installs the extra."""
+
+    @nox_poetry.session
+    def test(session: nox_poetry.Session) -> None:
+        """Install the local package."""
+        session.install(".[pygments]")
+
+    run_nox_with_noxfile(project, [test], [nox_poetry])
+
+    expected = [
+        project.package,
+        *project.dependencies,
+        project.get_dependency("pygments"),
+    ]
+    packages = list_packages(project, test)
+
+    assert set(expected) == set(packages)
+
+
+def test_dependency(project: Project) -> None:
+    """It installs the pinned dependency."""
+
+    @nox_poetry.session
+    def test(session: nox_poetry.Session) -> None:
+        """Install the dependency."""
+        session.install("pyflakes")
+
+    run_nox_with_noxfile(project, [test], [nox_poetry])
+
+    expected = [project.get_dependency("pyflakes")]
+    packages = list_packages(project, test)
+
+    assert set(expected) == set(packages)
+
+
+def test_local_wheel_and_dependency(project: Project) -> None:
+    """It installs the wheel with pinned dependencies."""
+
+    @nox_poetry.session
+    def test(session: nox_poetry.Session) -> None:
+        """Install the dependency."""
+        session.install(".", "pyflakes")
+
+    run_nox_with_noxfile(project, [test], [nox_poetry])
+
+    expected = [
+        project.package,
+        *project.dependencies,
+        project.get_dependency("pyflakes"),
+    ]
+    packages = list_packages(project, test)
+
+    assert set(expected) == set(packages)
+
+
+def test_session_parametrize(project: Project) -> None:
+    """It forwards parameters to sessions."""
+
+    @nox_poetry.session
+    @nox.parametrize("n", [1, 2])
+    def test(session: nox_poetry.Session, n: int) -> None:
+        """Do nothing."""
+
+    run_nox_with_noxfile(project, [test], [nox, nox_poetry])
+
+
+def test_url_dependency(shared_datadir: Path) -> None:
+    """It installs the package."""
+    project = Project(shared_datadir / "url-dependency")
+
+    @nox_poetry.session
+    def test(session: nox_poetry.Session) -> None:
+        """Install the local package."""
+        session.install(".")
+
+    run_nox_with_noxfile(project, [test], [nox_poetry])
+
+    expected = [project.package, *project.dependencies]
+    packages = list_packages(project, test)
+
+    assert set(expected) == set(packages)
+
+
+# https://github.com/python-poetry/poetry/issues/3468
+@pytest.mark.xfail(reason="Poetry exports path requirements in an invalid format.")
+def test_path_dependency(shared_datadir: Path) -> None:
+    """It installs the package."""
+    project = Project(shared_datadir / "path-dependency")
+
+    @nox_poetry.session
+    def test(session: nox_poetry.Session) -> None:
+        """Install the local package."""
+        session.install(".")
+
+    run_nox_with_noxfile(project, [test], [nox_poetry])
+
+    expected = [project.package, *project.dependencies]
+    packages = list_packages(project, test)
+
+    assert set(expected) == set(packages)
+
+
+def test_no_install(project: Project) -> None:
+    """It skips installation when --no-install is passed."""
+
+    @nox_poetry.session
+    def test(session: nox_poetry.Session) -> None:
+        """Install the local package."""
+        session.install(".")
+
+    run_nox_with_noxfile(project, [test], [nox_poetry])
+    run_nox_with_noxfile(project, [test], [nox_poetry], "-R")
+
+    expected = [project.package, *project.dependencies]
+    packages = list_packages(project, test)
+
+    assert set(expected) == set(packages)
+
+
+def test_poetry_warnings(shared_datadir: Path) -> None:
+    """It writes warnings from Poetry to the console."""
+    project = Project(shared_datadir / "outdated-lockfile")
+
+    @nox_poetry.session
+    def test(session: nox_poetry.Session) -> None:
+        """Install the local package."""
+        session.install(".")
+
+    process = run_nox_with_noxfile(project, [test], [nox_poetry])
+
+    assert "Warning:" in process.stderr
