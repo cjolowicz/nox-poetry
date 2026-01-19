e6766210f35739e4028019cf1c6306637b5a0acb
Edgar Ramírez Mondragón <edgarrm358@gmail.com>
2026-01-19 15:06:52 -0600

    Test with Poetry 2.3.0
    Signed-off-by: Edgar Ramírez Mondragón <edgarrm358@gmail.com>

--- noxfile.py
+++ noxfile.py
@@ -181,7 +181,8 @@ def mypy(session: Session) -> None:
         (python_versions[0], "poetry==1.8.5"),
         (python_versions[0], "poetry==2.0.1"),
         (python_versions[0], "poetry==2.1.4"),
-        (python_versions[0], "poetry==2.2.0"),
+        (python_versions[0], "poetry==2.2.1"),
+        (python_versions[0], "poetry==2.3.0"),
         *((python, None) for python in python_versions),
     ],
 )
